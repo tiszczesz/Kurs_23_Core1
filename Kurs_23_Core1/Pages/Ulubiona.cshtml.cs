769a9edcae52a887d24fabdf148ba8627bdf6359
Paweł Szczeszek <pszczesz@poczta.onet.pl>
2019-04-06 11:41:09 +0100

    first

---
+++ Kurs_23_Core1/Pages/Ulubiona.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kurs_23_Core1.Pages
+{
+    public class UlubionaModel : PageModel
+    {
+        public void OnGet()
+        {
+            ViewData["text"] = "kocham moją ....";
+        }
+    }
+}
--- Kurs_23_Core1/Startup.cs
+++ Kurs_23_Core1/Startup.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Kurs_23_Core1
@@ -15,6 +17,7 @@ namespace Kurs_23_Core1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +28,11 @@ namespace Kurs_23_Core1
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+           // app.UseDefaultFiles();
+            
+            app.UseMvc();
         }
     }
 }
